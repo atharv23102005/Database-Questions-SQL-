460e7ea80f93bfaff3ead3ceaec9fa7f6cc5893c
Atharva Srivastava <atharvasrivastava2023@gmail.com>
2026-08-12 23:54:34 +0530

    Time: 2 ms (36.1%), Space: 10.4 MB (89.03%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+
+        recurPermute(0, ans, nums);
+
+        return ans;
+    }
+    void recurPermute(int index,
+                  vector<vector<int>> &ans,
+                  vector<int> &nums){
+        if(index == nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+    for(int i = index ; i < nums.size(); i++){
+        swap(nums[index], nums[i]);
+        recurPermute(index+1 , ans, nums);
+        swap(nums[index], nums[i]);
+    }}
+};
