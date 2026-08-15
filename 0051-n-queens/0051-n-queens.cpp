3e8d30cb44cf74eacf372e6302c814a4b4d05f57
Atharva Srivastava <atharvasrivastava2023@gmail.com>
2026-08-16 00:12:20 +0530

    Time: 0 ms (100%), Space: 10.3 MB (48.62%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    public: 
+    void solve(int col, vector<string>&board, vector<vector<string>> & ans, vector<int> &leftRow , vector<int> &upperDiagonal , vector<int>&lowerDiagonal, int n ){
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+        // Try every row for this column
+        for (int row = 0; row < n; row++) {
+
+            if (leftRow[row] == 0 &&
+                lowerDiagonal[row + col] == 0 &&
+                upperDiagonal[n - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[n - 1 + col - row] = 1;
+
+                // Move to next column
+                solve(col + 1, board, ans,
+                      leftRow, upperDiagonal, lowerDiagonal, n);
+
+                // Backtrack
+                board[row][col] = '.';
+
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[n - 1 + col - row] = 0;
+
+            }
+        }
+    }
+
+
+
+public:
+
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string>board(n);
+        string s(n, '.');
+        for(int i = 0 ; i <n ; i++){
+            board[i]=s;
+        }
+       vector<int> leftRow(n, 0),
+            upperDiagonal(2 * n - 1, 0),
+            lowerDiagonal(2 * n - 1, 0);
+        solve(0 , board, ans,leftRow, upperDiagonal, lowerDiagonal, n);
+        return ans ;
+         
+    }
+};
