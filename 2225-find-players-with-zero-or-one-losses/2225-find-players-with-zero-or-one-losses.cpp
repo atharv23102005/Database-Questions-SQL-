1824a3062f27b02bdea4bc77bd4a6f107354a89c
Atharva Srivastava <atharvasrivastava2023@gmail.com>
2026-08-19 20:56:02 +0530

    Time: 131 ms (60.94%), Space: 168.9 MB (73.44%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        unordered_map<int , int> lost_map;
 
-        unordered_map<int, int> lost_map;
-
-        // Count losses
-        for (int i = 0; i < matches.size(); i++) {
-            int loser = matches[i][1];
-            lost_map[loser]++;
-        }
-
-        vector<int> notlost;
-        vector<int> lostOnce;
-
-        // Find players with 0 or 1 loss
-        for (int i = 0; i < matches.size(); i++) {
+        for(int i =0 ; i < matches.size();i++){
+            int looser= matches[i][1];
+            lost_map[looser]++;
+            }
+            vector<int> notlost;
+            vector<int> lostOnce;
+            
+        for(int i = 0 ; i < matches.size(); i++){
 
             int winner = matches[i][0];
-            int loser = matches[i][1];
+            int looser = matches[i][1];
 
-            // Winner has never lost
-            if (lost_map.find(winner) == lost_map.end()) {
-                notlost.push_back(winner);
-                lost_map[winner] = 2;
-            }
-
-            // Player has exactly one loss
-            if (lost_map[loser] == 1) {
-                lostOnce.push_back(loser);
-            }
+        if(lost_map.find(winner) == lost_map.end()){
+            notlost.push_back(winner);
+            lost_map[winner]=2;
         }
-
+        if( lost_map[looser] == 1 ){
+            lostOnce.push_back(looser);
+        
+        }
+    }
+      
         sort(notlost.begin(), notlost.end());
         sort(lostOnce.begin(), lostOnce.end());
-
-        return {notlost, lostOnce};
+       return {notlost,lostOnce} ;
+        
+                
     }
 };
