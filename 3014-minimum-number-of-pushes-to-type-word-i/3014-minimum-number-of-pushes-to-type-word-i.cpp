373325db14f176a77a98aaca94d7b13c35ac7a92
Atharva Srivastava <atharvasrivastava2023@gmail.com>
2026-09-03 17:48:53 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.04%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int result = 0 ;
-        unordered_map<int , int > mp;
-        int assign_key=2;
-        for(char &ch : word){
-            if(assign_key >9){
-                assign_key = 2;
-            }
-            mp[assign_key]++;
-            result += mp[assign_key];
-            assign_key++;
-        }
-        return result ;
+       // int result = 0 ;
+        vector<int> mp(26, 0);
+      //  unordered_map<int , int > mp;
+      for(char &ch : word){
+        mp[ch-'a']++;
+
+      }
+       // int assign_key=2;
+      //  for(char &ch : word){
+      //      if(assign_key >9){
+      //          assign_key = 2;
+       //     }
+      //      mp[assign_key]++;
+      //      result += mp[assign_key];
+      //      assign_key++;
+     //   }
+    //    return result ;
+    sort(begin(mp), end(mp), greater<int>());
+    int result = 0 ;
+    for(int i = 0;i <26;i++){
+        int freq = mp[i];
+        int press= i/8 +1 ;
+       result += freq * press;
     }
+     return result ;
+    }  
+   
 };
