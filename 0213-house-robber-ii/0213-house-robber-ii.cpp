d823859f34e6d4b0bfa76d93ba67b1715135cf03
Atharva Srivastava <atharvasrivastava2023@gmail.com>
2026-09-06 23:44:32 +0530

    Time: 0 ms (100%), Space: 10.8 MB (53.29%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-int t[101];
-int solve(vector<int>&nums , int i , int n ){
-    if(i > n) return 0 ;
-    if(t[i]!= -1) return t[i];
-
-    int steal = nums[i] + solve(nums, i+2 ,n);
-    int skip = solve(nums, i+1 ,n);
-
-    return t[i] = max(steal , skip);
-
-   
-}
-
-
-    int rob(vector<int>& nums) {
+ int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1 ) return nums[0];
-        if(n==2) return max(nums[0], nums[1]);
-        memset(t ,-1, sizeof(t));
-      // take first jouse wiith oth index    
-        int take_0th_index_house = solve(nums,0,n-2);
-        memset(t ,-1, sizeof(t));
-// take secound house with first index 
-        int take_1st_index_house = solve(nums,1,n-1);
-        return max(take_0th_index_house , take_1st_index_house);
-       
+        vector <int> t(n+1,0);
+
+        t[0]=0;
+        for (int i=1 ; i <=n-1 ; i++ ){
+          int skip =t[i-1];
+          int take =nums[i-1]+((i-2)>= 0 ? t[i-2] : 0);
+        t[i] = max(skip, take);
+
+     }
+          int result1= t[n-1];
+          t = vector<int>(n+1,0);
+
+       t[0]=0;
+       t[1]=0;
+     for (int i=2; i<=n ; i++ ){
+         int skip =t[i-1];
+         int take =nums[i-1]+((i-2)>= 0 ? t[i-2] : 0);
+        t[i]=max(skip,take);
         
     }
+
+        int result2 = t[n];
+
+    return max(result1,result2);
+ }
 };
